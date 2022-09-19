9fd9b814b056f430556c6f37a9cdb2f0e4b5196d
Surej Joseph <josephs@shure.com>
2022-09-19 14:00:50 -0500

    Adding keymaps for source code navigation using lsp

--- lua/maps.lua
+++ lua/maps.lua
@@ -1,4 +1,5 @@
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- Do not yank with x
 keymap.set('n', 'x', '"_x')
@@ -11,6 +12,14 @@ keymap.set('n', '-', '<C-x>')
 keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>',       {silent = true})
-keymap.set('n', 'ss', ':split<Return><C-w>w',   {silent = true})
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w',  {silent = true})
+keymap.set('n', 'te', ':tabedit<Return>',       opts)
+keymap.set('n', 'ss', ':split<Return><C-w>w',   opts)
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w',  opts)
+
+-- lsp keymaps
+keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>',          opts)
+keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>',         opts)
+keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>',                opts)
+keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>',          opts)
+keymap.set('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',   opts)
+
