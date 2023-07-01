a4b3f7ae5cd3c55579b495debd45df7e8f2c089b
Surej Joseph <surej.aj@hotmail.com>
2023-07-01 17:20:23 -0500

    Replacing sumeoko_lua with lua_ls

--- after/plugin/lspconfig.rc.lua
+++ after/plugin/lspconfig.rc.lua
@@ -4,29 +4,6 @@ if (not status) then return end
 local on_attach = function (client, bufnr)
 end
 
-nvim_lsp.sumneko_lua.setup {
-  on_attach = on_attach,
-  settings = {
-    Lua = {
-      diagnostics = {
-        -- Get the language server to recognize the 'vim' global
-        globals = { ' vim' }
-      },
-
-      workspace = {
-        -- Make the server aware of neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      },
-
-      --Do no send telemetry data
-      telemetry = {
-        enabled = false,
-      }
-    }
-  }
-}
-
 nvim_lsp.clangd.setup {
 }
 
--- after/plugin/mason.rc.lua
+++ after/plugin/mason.rc.lua
@@ -12,7 +12,7 @@ require("mason").setup({
 })
 
 require("mason-lspconfig").setup({
-    ensure_installed = {
+    ensure_installed = { 
       "lua_ls",
       "clangd",
       "cmake",
